97b62860ec270b118e345a9bce87ff95721f3c02
Anton <Kravch-anton@mail.ru>
2021-10-11 10:39:44 +0300

    Test Storybook/Snapshot

---
+++ src/addItemForm.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {AddItemForm} from "./addItemForm";
+import {action} from "@storybook/addon-actions";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'TODOLIST/AddItemForm',
+    component: AddItemForm,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        addItemForm: {
+            description: "button clicked"
+        }
+    },
+} as ComponentMeta<typeof AddItemForm>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const AddItemFormStories = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+AddItemFormStories.args = {
+    addItemForm: action('button clicked')
+};
+
