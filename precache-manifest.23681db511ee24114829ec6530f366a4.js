6859d6baac39af1f26ecfae868aaf5fd789ac470
Kravchuk Anton <Kravch-anton@mail.ru>
2022-02-07 21:44:26 +0300

    Updates

---
+++ precache-manifest.23681db511ee24114829ec6530f366a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1b7b808e4e4fc57cbe4e184a14565e",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "9df20df9fe215eacb575",
+    "url": "/todoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "10335fa8e16e252a7a9a",
+    "url": "/todoList/static/js/2.6e6559c3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList/static/js/2.6e6559c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df20df9fe215eacb575",
+    "url": "/todoList/static/js/main.2aaf8930.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
